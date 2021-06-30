d74e4e6924a9ba9607f82ea8fa1aa1ed0a643913
Adam Rumpf <adam.rumpf@gmail.com>
2021-06-30 01:12:51 -0400

    Completed most of a string tag removal method.

--- src/tex_untag/tex_untag.py
+++ src/tex_untag/tex_untag.py
@@ -1,9 +1,12 @@
 """Defines the main TeX file processing script."""
 
-from ._version import __author__, __version__
+###
+if __name__ != "__main__":
+    from ._version import __author__, __version__
 
 import os
 import re
+import sys
 
 #-----------------------------------------------------------------------------
 
@@ -22,10 +25,51 @@ def _untag_string(s, tag):
     of the tag. For example, to remove all instances of the
         \\textit{...}
     tag, pass the argument 'textit'.
+
+    It is assumed that any instances of the given tag begin and end on the
+    same line.
     """
 
-    ###
-    return (s, 0)
+    count = 0 # number of removals made
+    out = "" # output string
+
+    # Define \tag{...} head regex pattern
+    head = "\\\\" + tag + "\\{"
+
+    # Split string at tag heads
+    parts = re.split(head, s)
+    out = parts[0]
+
+    # Look for closing brackets to match to tag heads
+    overflow = False # whether the line ends with an open bracket
+    i = 0 # current line part
+    while i < len(parts) - 1:
+        i += 1
+        depth = 1 # bracket depth
+        j = 0 # position within string segment
+
+        while depth > 0 and j < len(parts[i]) - 1:
+            j += 1
+            if parts[i][j] == "{" and parts[i][j-1:j+1] != "\{":
+                depth += 1
+            elif parts[i][j] == "}" and parts[i][j-1:j+1] != "\}":
+                depth -= 1
+
+        if depth == 0:
+            # If closing bracket was found, remove it
+            count += 1
+            out += parts[i][:j] + parts[i][j+1:]
+        else:
+            # Otherwise keep the full string and break
+            overflow = True
+            break
+
+    # If the line oveflowed, include the remainder unaltered
+    if overflow == True:
+        head = "\\" + tag + "{"
+        out += head + head.join(parts[i:])
+
+    return (out, count)
 
 #-----------------------------------------------------------------------------
 
@@ -46,12 +90,21 @@ def untag_file(fname, tag, comment=False):
     of the tag. For example, to remove all instances of the
         \\textit{...}
     tag, pass the argument 'textit'.
+
+    It is assumed that any instances of the given tag begin and end on the
+    same line.
     """
 
-    count = 0 # number of replacements made
+    count = 0 # number of removals made
 
     # Get file directory
     path = os.path.abspath(fname)
+    if os.path.isfile(path) == False:
+        sys.exit("Input path is not a file name.")
+    if os.path.exists(path) == False:
+        sys.exit("Input path does not exist.")
+
+    # Initialize output file
     outfile = path + ".tmp"
 
     # Write edits to a temporary file
@@ -76,3 +129,12 @@ def untag_file(fname, tag, comment=False):
     os.rename(outfile, path)
 
     return count
+
+#-----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    ###
+    ### Add a -r argument to process a directory.
+    #print(_untag_string("This is a \\textit{t\\textbf{e}st}.", "textit"))
+    #print(_untag_string("This is a \\textit{test}.", "textit"))
+    print(_untag_string("This is a \\textit{test.", "textit"))
