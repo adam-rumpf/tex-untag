e3e487a7845085c9d518608d9e4c59e21c19f758
Adam Rumpf <arumpf@floridapoly.edu>
2021-12-30 15:20:26 -0500

    Implementing a single-source metadata system.

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 import setuptools
+import codecs
+import os.path
 
-# Settings defined in setup.cfg
-setuptools.setup()
+# Get metadata from version file (additional metadata defined in setup.cfg)
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_attr(rel_path, attr):
+    for line in read("src/tex_untag/" + rel_path).splitlines():
+        if line.startswith(attr):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find '" + attr + "' string.")
+
+setuptools.setup(author=get_attr("_version.py", "__author__"),
+                 version=get_attr("_version.py", "__version__"),
+                 author_email=get_attr("_version.py", "_author_email"))
